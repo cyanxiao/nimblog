a8a7d4d173249f4b5cf57753ccb02cd1c82c7b2a
Chell <im.chell@outlook.com>
2022-05-16 16:07:29 +0800

    fix: use existing html tag

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import * as ReactDOM from 'react-dom/client';
 import React from 'react';
 import Home from './components/Home';
 
-const rootNode = document.body;
+const rootNode = document.getElementById('menu');
 const root = ReactDOM.createRoot(rootNode);
-root.render(React.createElement(Home));
 
-export default function hello() {
-  console.log('hello');
+function renderBlog() {
+  root.render(React.createElement(Home));
 }
+
+renderBlog();
